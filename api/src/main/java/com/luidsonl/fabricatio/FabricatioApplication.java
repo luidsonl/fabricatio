f62415e3d2d3fa2565d1c6d1443c0ddb514aa92c
Luidson Lima <luidsonlsti@gmail.com>
2026-02-28 12:38:55 -0300

    initialize spring

---
+++ api/src/main/java/com/luidsonl/fabricatio/FabricatioApplication.java
@@ -0,0 +1,13 @@
+package com.luidsonl.fabricatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricatioApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/luidsonl/fabricatio/FabricatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luidsonl.fabricatio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

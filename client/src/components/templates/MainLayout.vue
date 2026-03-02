62fecf6187ef1251e14e0a77c26462dbce515e32
Luidson Lima Santos <luidsonlsti@gmail.com>
2026-03-02 20:36:57 -0300

    feat: add MainLayout template

---
+++ client/src/components/templates/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from '../molecules/Header.vue'
+</script>
+
+<template>
+  <div>
+    <Header />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
